b884f12ce716d3a64bdeeb12c69a7b3b7c71346e
dayhmk <dayhomework@gmail.com>
2016-02-20 10:19:29 -0500

    Switched to HTTPS

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # dayhmk.github.io
-A homework site for Day Middle School.
+A homework site for Day Middle School, created by the students of Day Middle School.
 Uses Bootstrap. Theme from Bootswatch. Icon from IconArchive.
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,10 @@
 var url = "https://dayhmkproxy.herokuapp.com/";
 
+// Force HTTPS
+if (window.location.protocol != "https:" && window.location.protocol != "file:"){
+    window.location.href = "https:"+window.location.href.substring(window.location.protocol.length);
+}
+
 function editText(text){
     var out = text.replace(new RegExp("quizlet","gi"),'<a href="https://quizlet.com/">Quizlet</a>');
     out = out.replace(new RegExp("schoology","gi"),'<a href="http://schoology.newton.k12.ma.us/login/ldap?&school=105120621">Schoology</a>');
